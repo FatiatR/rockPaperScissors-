1d2775fa59d34397e6e0f508044c8ad5775a3637
FatiatR <181467547+FatiatR@users.noreply.github.com>
2024-10-14 05:44:40 +0100

    alk code added

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
-//Write a function that gets the Computer choice
-function getComputerChoice(rock, paper,scissors) 
-{ 
-const choice1=rock ;
-const choice2=paper;
-const choice3=scissors ;
-let choices=["choice1","choice2","choice3"];
-const randomIndex=Math.floor(Math.random() * 3);
- return choices[randomIndex];
- }
- 
+//Write a function that get the computer choice
+//I use the const to declare the choices sunce they will be constant through the game.
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
+}
+
  //Write a function that takes the human choice and returns it
- 
- function getHumanChoice(humanChoice,computerChoice)
- {
-     //if human choice is the same thing as computerchoice, return draw
-     if (humanChoice===computerChoice){
-         return "Draw!!";
-     }
-     //if 
-     else if((humanChoice=== "rock" && computerChoice=== "scissors") || 
-     (humanChoice=== "scissors" && computerChoice=== "paper") || 
-       (humanChoice=== "paper" && computerChoice=== "rock"))
-     
-     
-     {
-         return "You win!";
-     }
-     else{
-         return "You lose!";
-     }
- }
- 
- //Declaring the human score 
- let humanScore= 0;
- let humanChoice;
- 
- //A function that takes human and computer choices
- humanChoice=prompt("Enter: 'rock','paper','scissors':");
- 
- while (humanChoice !== "rock" && humanChoice !== "scissors" && humanChoice !== "paper")
- {
- humanChoice= prompt("Invalid Choice.Enter: 'rock','paper','scissors':")
-      
- }
- const computerChoice= getComputerChoice();
- console.log("You chose: " + humanChoice);
- console.log("computerChoice: " + computerChoice);
- 
- 
- const result =getHumanChoice(humanChoice, computerChoice);
- console.log(result);
- 
- if (result=== "You win!")
- {
-     humanScore++;
- }
- const continuePlaying=prompt("Do you want to play again? (Yes/No)") ;
- //Display final score
- console.log(" Final Score: + You won " + humanScore + " times!");
+ //Using the if,else and else if function.If the humanChoice and computerChoice is the same return draw. 
+function getHumanChoice(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return "Draw!!";
+  } else if ((humanChoice === "rock" && computerChoice === "scissors") || 
+             (humanChoice === "scissors" && computerChoice === "paper") || 
+             (humanChoice === "paper" && computerChoice === "rock")) {
+    return "You win!";
+  } else {
+    return "You lose!";
+  }
+}
+
+//Then i declared the initial score for both computerChoice and humanCjoice by using the 'let' 
+let humanScore = 0;
+let computerScore = 0;
+
+// Then I wrote the logic of the game and also making sure my humanChoice is case insensitive,that is Rock,rock,RoCk is acceptable 
+while (true) {
+  // Get human choice
+  let humanChoice = prompt("Enter: 'rock','paper','scissors':");
+  while (humanChoice !== "rock" && humanChoice !== "scissors" && humanChoice !== "paper") {
+    humanChoice = prompt("Invalid Choice.Enter: 'rock','paper','scissors':");
+  
+  }
+
+  // Get computer choice
+  const computerChoice = getComputerChoice();
+
+  // Show the humanChoice and computerChouce
+  console.log("You chose: " + humanChoice);
+  console.log("Computer chose: " + computerChoice);
+
+  // Calculate the game result 
+  const playRound = getHumanChoice(humanChoice, computerChoice);
+  console.log(playRound);
+
+
+  if (playRound === "You win!") {
+    humanScore++;
+  } else if (playRound !== "Draw!!") {
+    computerScore++;
+  }
+
+  // Thus will show do you want to continue playing? question,then you can pick Yes/No
+  const continuePlaying = prompt("Do you want to continue playing? (Yes/No)");
+  
+}
+
+// Show the final score
+console.log("Final Score: You won " + humanScore + " times, Computer won " + computerScore + " times!");
